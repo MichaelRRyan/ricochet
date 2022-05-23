7cb26d2ea44e2348ec8f7a6dbc9cdbf8dd439e52
Michael R Ryan <mrainsfordryan@gmail.com>
2022-05-23 19:46:48 +0100

    Added a basic player and ricocheting bullets

---
+++ scripts/bullet.gd
@@ -0,0 +1,30 @@
+extends Area2D
+
+const SPEED = 1000.0
+var velocity = Vector2(SPEED, 0.0)
+
+
+#-------------------------------------------------------------------------------
+func set_direction(direction):
+	velocity = direction * SPEED
+	rotation = atan2(velocity.y, velocity.x)
+
+
+#-------------------------------------------------------------------------------
+func _physics_process(delta):
+	var movement = velocity * delta
+	
+	$RayCast2D.cast_to = movement.rotated(-rotation)
+	$RayCast2D.force_raycast_update()
+	if $RayCast2D.is_colliding():
+		
+		var normal = $RayCast2D.get_collision_normal()
+		if Vector2.ZERO != normal:
+			
+			set_direction(velocity.bounce(normal).normalized())
+			movement = velocity * delta
+		
+	position += movement
+
+
+#-------------------------------------------------------------------------------
---
+++ scripts/player.gd
@@ -0,0 +1,33 @@
+extends KinematicBody2D
+
+const SPEED = 100.0
+const BulletScene = preload("res://scenes/bullet.tscn")
+
+
+#-------------------------------------------------------------------------------
+func _physics_process(_delta):
+	var movement = Vector2.ZERO
+	
+	if Input.is_action_pressed("move_left"):
+		movement.x -= SPEED
+	if Input.is_action_pressed("move_right"):
+		movement.x += SPEED
+	if Input.is_action_pressed("move_up"):
+		movement.y -= SPEED
+	if Input.is_action_pressed("move_down"):
+		movement.y += SPEED
+	
+	move_and_slide(movement)
+	
+	
+#-------------------------------------------------------------------------------
+func _input(event):
+	if event.is_action_pressed("shoot"):
+		var bullet = BulletScene.instance()
+		get_parent().add_child(bullet)
+		var direction = (get_global_mouse_position() - global_position).normalized()
+		bullet.set_direction(direction)
+		bullet.position = global_position
+
+
+#-------------------------------------------------------------------------------
