27f795cdb9ea6abbe4912eb1f4797c73c49f3c02
Michael R Ryan <mrainsfordryan@gmail.com>
2022-05-25 19:37:47 +0100

    Added levels 1 to 5 and level switching

--- scripts/gameplay_manager.gd
+++ scripts/gameplay_manager.gd
@@ -4,6 +4,8 @@ signal level_complete()
 signal level_failed()
 
 
+export var level_number : int = -1
+
 var enemy_count = -1
 
 
@@ -31,6 +33,19 @@ func _on_enemy_died():
 func _on_player_died():
 	emit_signal("level_failed")
 	get_tree().paused = true
+
+
+#-------------------------------------------------------------------------------
+func _on_LevelCompleteScreen_next_level_button_pressed():
+	var level_path = "res://scenes/levels/level" + str(level_number + 1) + ".tscn"
+	if ResourceLoader.exists(level_path):
+		if get_tree().change_scene(level_path) != OK:
+			print("Error switching to the next leve.")
+			
+		get_tree().paused = false
+		
+	else:
+		print("No 'level " + str(level_number + 1) + "' found.")
 
 
 #-------------------------------------------------------------------------------
--- scripts/ui/level_complete_screen.gd
+++ scripts/ui/level_complete_screen.gd
@@ -1,5 +1,7 @@
 extends Control
 
+signal next_level_button_pressed()
+
 
 #-------------------------------------------------------------------------------
 func _on_GameplayManager_level_complete():
@@ -7,9 +9,17 @@ func _on_GameplayManager_level_complete():
 
 
 #-------------------------------------------------------------------------------
+func _on_NextLevelButton_pressed():
+	emit_signal("next_level_button_pressed")
+	
+	
+#-------------------------------------------------------------------------------
 func _on_RetryButton_pressed():
 	var _r = get_tree().reload_current_scene()
 	get_tree().paused = false
 
 
 #-------------------------------------------------------------------------------
+
+
+
